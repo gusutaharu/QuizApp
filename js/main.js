fd02f7ff57174898f366e6672c33db64c6dbd417
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-10-22 09:51:50 +0900

    span要素が一つの選択肢に２以上表示されるのを修正

--- js/main.js
+++ js/main.js
@@ -4,13 +4,16 @@
 
   choices.forEach((choice)=>{
     choice.addEventListener('click',()=>{
-      const answer = document.createElement('span');
-      if (choice.classList.contains('correct')){
-        answer.textContent = ' - 正解!'
-        choice.append(answer);
-      }else{
-        answer.textContent = ' - 不正解...'
-        choice.append(answer);
+      if(choice.classList.contains('clicked')===false){
+        choice.classList.add('clicked');
+        const answer = document.createElement('span');
+        if (choice.classList.contains('correct')){
+          answer.textContent = ' - 正解!'
+          choice.appendChild(answer);
+        }else{
+          answer.textContent = ' - 不正解...'
+          choice.appendChild(answer);
+        }
       }
     })
   });
