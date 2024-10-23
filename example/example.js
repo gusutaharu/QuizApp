a33fc1f067dd4f34df98d6ec04e580271f4400c9
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-10-23 09:23:35 +0900

    lesson06

--- example/example.js
+++ example/example.js
@@ -1,4 +1,14 @@
 'use strict'
 {
+  function render(quiz) {
+    const main = document.querySelector('main');
+
+    const section = document.createElement('section');
+
+    main.appendChild(section);
+  }
+
   const quiz = ['1の正解は？', '選択肢 A', '選択肢 B', '選択肢 C', 0];
+
+  render(quiz);
 }
